1369af736811ec161a020c21b034f26bef29fcc3
yashDumasia <dumasiayash1897@gmail.com>
2025-05-22 14:24:31 +0530

    some changes in Testing file

--- Testing.py
+++ Testing.py
@@ -1,2 +1,11 @@
 Name = "YASH DUMASIA"
 print(len(Name))
+print(Name[0])
+print(Name[1])
+print(Name[2])
+print(Name[3])
+print(Name[4])
+print(Name[0:4])
+print(Name[:4])
+print(Name[0:])
+print(Name[:])
