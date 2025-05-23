bc01801490a441be53a01cc5ed080d5be7d08c93
yashDumasia <dumasiayash1897@gmail.com>
2025-05-23 15:10:27 +0530

    some changes : add Number function in Number file

--- Number.py
+++ Number.py
@@ -1,7 +1,2 @@
-print(10+3)
-print(10-3)
-print(10*3)
-print(10**3)  # 10 rest to 3 = 1000(3 the power of 10)
-print(10/3)  # return floating value
-print(10//3)  # return integer value
-print(10 % 3)  # return remainder of division
+print(round(5.7))  # round of number
+print(abs(-5.7))  # return positive number
