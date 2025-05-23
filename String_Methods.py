7fb9c01a80bed3cac61bbfaabbce90b22e7af524
yashDumasia <dumasiayash1897@gmail.com>
2025-05-23 13:13:54 +0530

    add String Methods

--- String_Methods.py
+++ String_Methods.py
@@ -3,9 +3,9 @@ print(Name)
 print(Name.title())  # first later capital in word
 print(Name.upper())  # every later capital in word
 print(Name.lower())  # every later smaller in word
-print(Name.strip())  # remeved blank space in beginnig of word
-print(Name.lstrip())  # remeved blank space in beginnig of word
-print(Name.rstrip())  # remeved blank space in end of word
+print(Name.strip())  # removed blank space in beginnig of word
+print(Name.lstrip())  # removed blank space in beginnig of word
+print(Name.rstrip())  # removed blank space in end of word
 print(Name.find("Dum"))  # return index of charcter
 print(Name.replace("D", "d"))  # repalce "D" into "d"
 print("Duma" in Name)  # return boolean(true\false) of charcter
