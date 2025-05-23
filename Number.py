25086c5b87f6028aab8499d1c271d5dce2fdd5ab
yashDumasia <dumasiayash1897@gmail.com>
2025-05-23 13:31:34 +0530

    add Number file : use of +,-,*,**,/,//,%

---
+++ Number.py
@@ -0,0 +1,7 @@
+print(10+3)
+print(10-3)
+print(10*3)
+print(10**3)  # 10 rest to 3 = 1000(3 the power of 10)
+print(10/3)  # return floating value
+print(10//3)  # return integer value
+print(10 % 3)  # return remainder of division
