5cb9a336ef50e7e42a2da1732967709fa8558ea9
yashDumasia <dumasiayash1897@gmail.com>
2025-05-24 16:01:47 +0530

    add Temperature file

---
+++ Tempreture.py
@@ -0,0 +1,8 @@
+Tempreture = input("Tempreture : ")
+if int(Tempreture) >= 35:
+    print("It's worm.")
+elif int(Tempreture) >= 20:
+    print("It's nice.")
+else:
+    print("It's cold.")
+print("Thanks")
--- Testing.py
+++
@@ -1,3 +0,0 @@
-x = input("x : ")
-y = float(x) + 5
-print(y)
