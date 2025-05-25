abf23c320a16c47891a15add443fc98f61330889
yashDumasia <dumasiayash1897@gmail.com>
2025-05-25 10:45:54 +0530

    some changes in Tempreture file

--- Tempreture.py
+++ Tempreture.py
@@ -1,9 +1,4 @@
 Tempreture = input("Tempreture : ")
-if int(Tempreture) >= 35:
-    message = "It's worm."
-elif int(Tempreture) >= 20:
-    message = "It's nice."
-else:
-    message = "It's cold."
+message = "It's worm." if int(Tempreture) >= 30 else "It's cold."
 print(message)
 print("Thanks")
