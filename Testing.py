57fa015d316b223589c603e1f3bd427037fcb62e
yashDumasia <dumasiayash1897@gmail.com>
2025-05-26 12:42:50 +0530

    some changes in Testing file

--- Testing.py
+++ Testing.py
@@ -1,18 +1,2 @@
-high_income = True
-good_credit = True
-student = False
-# and oprator
-if high_income and good_credit == True:
-    print("Eligible")
-else:
-    print("Not Eligible")
-# or oprator
-if high_income or good_credit == True:
-    print("Eligible")
-else:
-    print("Not Eligible")
-# not oprator
-if not student == True:
-    print("Eligible")
-else:
-    print("Not Eligible")
+for x in ("YASH"):
+    print(x)
