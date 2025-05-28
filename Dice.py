aacac2620b65761fe984b9126d7a39022490e5cd
yashDumasia <dumasiayash1897@gmail.com>
2025-05-28 13:13:15 +0530

    some changes : add one more Dice

--- Dice.py
+++ Dice.py
@@ -3,7 +3,8 @@ while True:
     choice = input("Roll the Dice(y/n)...? : ")
     if choice.lower() == "y":
         dice1 = random.randint(1, 6)
-        print(f"({dice1})")
+        dice2 = random.randint(1, 6)
+        print(f"({dice1},{dice2})")
     elif choice.lower() == "n":
         print("Thanks for playing")
         break
