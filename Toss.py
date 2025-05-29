f0110de3c15cb5b23dcd45cc2ffaeb74df0a7a56
yashDumasia <dumasiayash1897@gmail.com>
2025-05-29 15:28:44 +0530

    some changes in Toss file

--- Toss.py
+++ Toss.py
@@ -1,14 +1,24 @@
 import random
+x = ["Head", "Tail"]
 while True:
-    choice = input("Toss(y/n) : ")
-    if choice.lower() == "y":
-        Toss = random.randint(1, 2)
-        if Toss == 1:
-            print("Head")
-        else:
-            print("Tail")
-    elif choice.lower() == "n":
-        print("Thanks for playing")
+    choice = input("Toss(H/T) : ").upper()
+    Toss = random.choice(x)
+    if choice == "H":
+        print("You chose Head")
+    elif choice == "T":
+        print("You chose Tail")
+    else:
+        print("Invalid Choice..!")
+    print(f"Toss = {Toss}")
+    if choice == "H" and Toss == "Head" or choice == "T" and Toss == "Tail":
+        print("You won Toss.")
+    else:
+        print("You loss Toss.")
+    play = input("Do you want to Toss agian...?(Y/N) : ").upper()
+    if play == "Y":
+        continue
+    elif play == "N":
         break
     else:
-        print("Invalid Choise...! ")
+        print("Invalid Choice..!")
+        break
